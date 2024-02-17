8c8f9eb7796eec0bbf9ad92f5ad3e46278cf2070
abaduna~ <artuelrey@gmail.com>
2024-02-17 20:36:52 -0300

    termiando el validacion front

--- pagina_ya/cliente/src/pages/login/iniciar/Inicia.js
+++ pagina_ya/cliente/src/pages/login/iniciar/Inicia.js
@@ -2,35 +2,52 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { useForm } from "react-hook-form";
+import Alert from "react-bootstrap/Alert";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [contraseña, setContraseña] = useState("");
-  const inicio = () => {};
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const inicio = (object) => {
+    console.log(object);
+  };
   return (
     <>
       <h4>inicia secion</h4>
-      <form>
+      <form onSubmit={handleSubmit(inicio)}>
+        {errors?.usuario && (
+          <Alert variant="danger">El campo es obligatorio</Alert>
+        )}
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="Usuario"
-            value={username}
             aria-label="Usuario"
             aria-describedby="basic-addon1"
-            onChange={(e) => setUsername(e.target.value)}
+            {...register("usuario", {
+              required: true,
+            })}
           />
         </InputGroup>
+        {errors?.contraseña && (
+          <Alert variant="danger">El campo es obligatorio</Alert>
+        )}
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="Contraseña"
             aria-label="Contraseña"
             aria-describedby="basic-addon1"
-            value={contraseña}
-            onChange={(e) => setContraseña(e.target.value)}
+            {...register("contraseña", {
+              required: true,
+            })}
           />
         </InputGroup>
-        
-        <Button onClick={inicio}>Iniciar secion</Button>
+
+        <Button type="submit">Iniciar secion</Button>
       </form>
     </>
   );
--- pagina_ya/cliente/src/pages/login/register/Register.js
+++ pagina_ya/cliente/src/pages/login/register/Register.js
@@ -3,45 +3,69 @@ import Button from "react-bootstrap/esm/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import NavBar from "../../../componets/Navbar/NavBar";
+import { useForm } from "react-hook-form";
+import Alert from "react-bootstrap/Alert";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [contraseña, setContraseña] = useState("");
-  const [reptcontraseña, setReptcontraseña] = useState("");
-  const registrar = () => {};
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm();
+  const registrar = (object) => {
+    console.log(object);
+  };
+  const password = watch("contraseña", "");
   return (
     <>
       <NavBar />
       <h4>Registrate</h4>
-      <form>
+      <form onSubmit={handleSubmit(registrar)}>
+        {errors?.usuario && (
+          <Alert variant="danger">El campo es obligatorio</Alert>
+        )}
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="Usuario"
-            value={username}
             aria-label="Usuario"
             aria-describedby="basic-addon1"
-            onChange={(e) => setUsername(e.target.value)}
+            {...register("usuario", {
+              required: true,
+            })}
           />
         </InputGroup>
+        {errors?.contraseña && (
+          <Alert variant="danger">El campo es obligatorio</Alert>
+        )}
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="Contraseña"
             aria-label="Contraseña"
             aria-describedby="basic-addon1"
-            value={contraseña}
-            onChange={(e) => setContraseña(e.target.value)}
+            {...register("contraseña", {
+              required: true,
+            })}
           />
         </InputGroup>
+        {errors?.repcontraseña && (
+          <Alert variant="danger">La contraseña no coinciden</Alert>
+        )}
         <InputGroup className="mb-3">
           <Form.Control
             placeholder="Repeti contraseña"
             aria-label="Repeti contraseña"
             aria-describedby="basic-addon1"
-            value={reptcontraseña}
-            onChange={(e) => setReptcontraseña(e.target.value)}
+            {...register("repcontraseña", {
+              required: true,
+              validate: (value) =>
+                value === password || "La contraseña no coinciden",
+              
+            })}
           />
         </InputGroup>
-        <Button onClick={registrar}>Iniciar secion</Button>
+        <Button type="submit">Iniciar secion</Button>
       </form>
     </>
   );
